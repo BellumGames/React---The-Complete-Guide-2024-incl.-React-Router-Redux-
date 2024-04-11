8cd9c24f850767dbc248917ad5afdfc11fdf0fd7
Dan-Alexandru Bucur <bucur.alexandru.dan@gmail.com>
2024-04-11 17:06:06 +0300

    Update after a pause

--- Section 03/src/components/TabButton.jsx
+++ Section 03/src/components/TabButton.jsx
@@ -23,3 +23,28 @@ export default function Card({ name, children }) {
 
 // From tests before
 */
+
+/*
+// IMPORTANT: You CAN'T import & use useState like this in this Udemy environment
+// import { useState } from 'react'
+// Instead, import & use it like this:
+// import React from 'react';
+// React.useState();
+
+import React from 'react';
+ 
+export default function App() {
+    const [price, setPrice] = React.useState(100); // 100 is the default price state
+ 
+    function handleClick() {
+      setPrice(75);
+    }
+ 
+    return (
+        <div>
+            <p data-testid="price">${price}</p>
+            <button onClick={handleClick}>Apply Discount</button>
+        </div>
+    );
+}
+*/
