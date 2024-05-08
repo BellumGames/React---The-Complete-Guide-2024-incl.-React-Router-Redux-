baf5cedf28684ab0277bb90832195dc369256b1c
Dan-Alexandru Bucur <bucur.alexandru.dan@gmail.com>
2024-05-08 21:20:11 +0300

    Tested out dynamical arrays

--- Section 03/src/App.jsx
+++ Section 03/src/App.jsx
@@ -98,3 +98,33 @@ export default App;
 <CoreConcept {...CORE_CONCEPTS[2]} />
 <CoreConcept {...CORE_CONCEPTS[3]} />
 */
+
+/*
+import React from 'react';
+
+import Todo from './Todo';
+
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+
+// don't change the Component name "App"
+export default function App() {
+    return(
+        <div>
+            {DUMMY_TODOS.map((item) => (
+                <Todo text={item} />
+            ))}
+        </div>
+    );
+}
+
+import React from 'react';
+
+export default function Todo({text}) {
+    return <li>{text}</li>;
+}
+*/
